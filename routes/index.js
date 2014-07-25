cda8e6e7dc24bee43acbece3080a29825e45b619
rpflynn22 <rpflynn22@gmail.com>
2014-07-25 11:28:20 -0700

    non duplicating signup

--- routes/index.js
+++ routes/index.js
@@ -12,56 +12,6 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-  mongodb.Db.connect(process.env.MONGOHQ_URL, function(err, db) {
-    var collection = db.collection('test');
-    console.log('removing documents...');
-    collection.remove(function(err, result) {
-      if (err) {
-        res.write("first level error");
-        res.end();
-        return console.error(err);
-      }
-      console.log('collection cleared!');
-      console.log('inserting new docs');
-      collection.insert([{name: 'tester'}, {name: 'coder'}], function(err, docs) {
-        if (err) {
-          res.write("second level error");
-          res.end();
-          return console.error(err);
-        }
-        console.log('just inserted ', docs.length, ' new docs!');
-        collection.find({}, {}, function(err, docs) {
-          if (err) {
-            res.write("third level error");
-            res.end();
-            return console.error(err);
-          }
-          res.write(JSON.stringify(docs));
-          res.end();
-        });
-      });
-    });
-  });
-});
-
-/* GET new user page. */
-router.get('/newuser', function(req, res) {
-  res.render('newuser', { title: 'Add New User' });
-});
-
-/* POST to add User Service. */
-router.post('/adduser', function(req, res) {
-  var db = req.db;
-  var collection = db.get('usercollection');
-  collection.insert(req.body, function(err, doc) {
-    res.send(
-      (err === null) ? { msg: '' } : { msg: err }
-    );
-  });
-});
-
 /* GET add file page. */
 router.get('/newfile', function(req, res) {
   res.render('newfile', { title: 'Upload File' });
@@ -75,20 +25,25 @@ router.post('/uploadfile', function(req, res) {
   });
 });
 
+/* GET for android devices to sign up. Should probably be a post. */
 router.get('/signup', function(req, res) {
   mongodb.Db.connect(process.env.MONGOHQ_URL, function(err, db) {
     var collection = db.collection('phone-users');
     var droid_id = req.param('android_id');
-    collection.insert({android_id: droid_id}, function(err, result) {
-      if (err) {
-        return console.error(err);
+    collection.find({android_id: droid_id}, {fields:{_id:0}}, function(err, doc) {
+      if (doc.length == 0) {
+        collection.insert({android_id: droid_id}, function(err, result) {
+          if (err) {
+            return console.error(err);
+          }
+          res.send(200);
+        });
+      } else {
+        res.end("id exists");
       }
-      res.send(200);
     });
   });
-});
-      
-  
+});  
 
 /* GET for reading files. */
 router.get('/readfile', function(req, res) {
@@ -100,30 +55,31 @@ router.get('/readfile', function(req, res) {
       // DO STUFF!!!!
       var filepath = "./uploads/" + files[0];
       var droid_id = req.param("android_id");
-      var db = req.db;
-      var collection = db.get('usercollection');
-      collection.find({}, {fields:{android_id:0}}, function(err, results) {
-        var idarr = Array();
-        for (i = 0; i < results.length; i++) {
-          idarr.push(results[i]._id.toString());
-        }
-        idarr.sort();
-        collection.find({android_id:droid_id}, {fields:{android_id:0}}, function(err, target) {
-          var index = idarr.indexOf(target[0]._id.toString());
-          fs.readFile(filepath, function(err, data) {
-            if (err) throw err;
-            var linesArray = String(data).split("\n");
-            var linesPerPhone = Math.floor(linesArray.length / idarr.length);
-            var rem = linesArray.length % idarr.length;
-            var myStartLine = index * linesPerPhone;
-            if (index == (idarr.length - 1)) linesPerPhone += rem;
-            var specLines = linesArray.slice(myStartLine, myStartLine + linesPerPhone);
-            var output = specLines.join("\n");
-            res.write(output);
-            res.end();
+      mongodb.Db.connect(process.env.MONGOHQ_URL, function(err, db) {
+        var collection = db.collection('phone-users');
+        collection.find({}, {fields:{android_id:0}}, function(err, results) {
+          var idarr = Array();
+          for (i = 0; i < results.length; i++) {
+            idarr.push(results[i]._id.toString());
+          }
+          idarr.sort();
+          collection.find({android_id:droid_id}, {fields:{android_id:0}}, function(err, target) {
+            var index = idarr.indexOf(target[0]._id.toString());
+            fs.readFile(filepath, function(err, data) {
+              if (err) throw err;
+              var linesArray = String(data).split("\n");
+              var linesPerPhone = Math.floor(linesArray.length / idarr.length);
+              var rem = linesArray.length % idarr.length;
+              var myStartLine = index * linesPerPhone;
+              if (index == (idarr.length - 1)) linesPerPhone += rem;
+              var specLines = linesArray.slice(myStartLine, myStartLine + linesPerPhone);
+              var output = specLines.join("\n");
+              res.write(output);
+              res.end();
+            });
           });
-        });
-      });        
+        });        
+      });
     }
   });
 });
