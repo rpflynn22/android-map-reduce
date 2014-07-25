fd045d7e1a705b94e30044b0a109e08b8a505c7c
rpflynn22 <rpflynn22@gmail.com>
2014-07-25 11:48:44 -0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.get('/signup', function(req, res) {
     var collection = db.collection('phone-users');
     var droid_id = req.param('android_id');
     collection.find({android_id: droid_id}, {fields:{_id:0}}, function(err, doc) {
-      res.write(JSON.stringify(doc));
+      res.write(typeof doc);
       //res.write(String(doc.length));
       if (typeof doc.length === 'undefined') {
         collection.insert({android_id: droid_id}, function(err, result) {
